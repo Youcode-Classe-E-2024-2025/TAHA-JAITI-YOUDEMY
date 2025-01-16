0c56943f02b5f5d465264ff5330af9ec0ac7848d
Taha <kyojin.rar@gmail.com>
2025-01-16 12:42:30 +0100

    FEAT: Extend StudentCourse model from Course and implement placeholder methods for save, getById, and getAll

--- App/Models/StudentCourse.php
+++ App/Models/StudentCourse.php
@@ -1,5 +1,21 @@
 <?php
 
-class StudentCourse {
-    
+class StudentCourse extends Course{
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function save(): bool{
+        throw new Exception('You dont have the permission');
+    }
+
+    public function getById(): Course|null{
+        return null;
+    }
+
+    public function getAll(): array{
+        return [];
+    }
+
 }
